95900e54fa320714c6c8ed73f713623aa0b740eb
Thiago Miziara <thiago.miziara@luizalabs.com>
2024-02-07 17:52:42 -0300

    feat: add controllers and drivers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,13 +1,15 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreateTagController
 
 class TagCreatorView:
     '''
         responsability for interacting with HTTP
     '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
-        print(http_request)
-        return HttpResponse(status_code=200,body={"hello": "world"})
+        tag_creator_controller = TagCreateTagController()
+        body = http_request.body
+        product_code = body["product_code"]
+        formatted_response = tag_creator_controller.create(product_code)
+        return HttpResponse(status_code=200,body=formatted_response)
     
