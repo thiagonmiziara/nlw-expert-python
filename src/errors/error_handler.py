49ce790e8d14a9dfffde96bb650f0e8b90512262
Thiago Miziara <thiago.miziara@luizalabs.com>
2024-02-08 18:27:42 -0300

    feat: add validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,9 +1,23 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
-def handle_errors(error: Exception) -> HttpResponse:
-    return HttpResponse(status_code=500, body={
-    "errors":[{
-      "title": "Server Error",
-      "detail": str(error)
-    }]
-  })
+def error_handler(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+    return HttpResponse(
+        status_code=500,
+        body={
+            "errors": [{
+                "title": "Server Error",
+                "detail": str(error)
+            }]
+        }
+    )
